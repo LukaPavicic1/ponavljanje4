6f0c98a5d594228b1de2911a3816c06934178d62
LukaPavicic1 <91119924+LukaPavicic1@users.noreply.github.com>
2022-09-12 12:52:24 +0200

    Add files via upload

---
+++ Vježbanje4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vježbanje4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y; 
+            int z;
+
+            Console.WriteLine("Upiši stranicu trokuta: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            z = Convert.ToInt32(Console.ReadLine());
+
+            if ((x * x) + (y * y) == (z * z) || (x * x) + (z * z) == (y * y) || (y * y) + (z * z) == (x * x))
+            {
+                Console.WriteLine("To je Pravokutni trokut\n");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("To nije pravokutni trokut\n");
+                Console.ReadKey();
+            }
+        }
+    }
+}
